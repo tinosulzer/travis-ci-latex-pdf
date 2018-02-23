39bf44cbba2a42076245acfe683da1fd55d31956
Thomas <hollandpirates@gmail.com>
2018-02-23 19:28:39 +0100

    use travis env var

--- test/compile-2.sh
+++ test/compile-2.sh
@@ -2,7 +2,7 @@
 
 # This file is used for testing this repo, replacing the corresponding .travis.yml which cannot be included.
 
-source ../2-texlive-pdflatex/texlive_install.sh
+source ${TRAVIS_BUILD_DIR}/2-texlive-pdflatex/texlive_install.sh
 
-travis_wait 3 pdflatex ../src/main.tex
-travis_wait 3 pdflatex ../src/main.tex
+travis_wait 3 pdflatex ${TRAVIS_BUILD_DIR}/src/main.tex
+travis_wait 3 pdflatex ${TRAVIS_BUILD_DIR}/src/main.tex
