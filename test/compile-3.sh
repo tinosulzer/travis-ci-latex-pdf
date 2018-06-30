3f7ae65f571de49f3210e360491c96305828822d
Thomas <hollandpirates@gmail.com>
2018-06-30 20:47:51 +0200

    Fix path to main

--- test/compile-3.sh
+++ test/compile-3.sh
@@ -4,4 +4,4 @@ set -e
 
 cd ${TRAVIS_BUILD_DIR}/src/
 Rscript ${TRAVIS_BUILD_DIR}/3-tinytex/install_texlive.R
-Rscript -e 'tinytex::pdflatex("./main.tex", bib_engine = "biber")'
+Rscript -e 'tinytex::pdflatex("main.tex", bib_engine = "biber")'
