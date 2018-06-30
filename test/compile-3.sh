521a28914935eee943b44bfa215590d2e1d8320b
Thomas <hollandpirates@gmail.com>
2018-06-30 20:43:40 +0200

    Change wd 3

--- test/compile-3.sh
+++ test/compile-3.sh
@@ -2,5 +2,6 @@
 # Any subsequent commands which fail will cause the shell script to exit immediately
 set -e
 
+cd ${TRAVIS_BUILD_DIR}/src/
 Rscript ${TRAVIS_BUILD_DIR}/3-tinytex/install_texlive.R
-Rscript -e 'tinytex::pdflatex("src/main.tex", bib_engine = "biber")'
+Rscript -e 'tinytex::pdflatex("./main.tex", bib_engine = "biber")'
