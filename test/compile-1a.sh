6eb60ce017562e4b6a7257454862501315e4fbbd
Manuel <wtfjoke@livenet.ch>
2018-10-11 17:31:36 +0200

    use new docker image with biber support

--- test/compile-1a.sh
+++ test/compile-1a.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Copied from 1a-tectonic-docker/.travis.yml without comments
-docker pull rekka/tectonic
-docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind rekka/tectonic tectonic main.tex
+docker pull dxjoke/tectonic-docker
+docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; tectonic biber-mwe.tex; tectonic main.tex"
