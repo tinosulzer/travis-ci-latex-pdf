0ca054967bac5ab220b8d17bb677b4ad74740a35
Thomas <hollandpirates@gmail.com>
2018-11-05 09:58:22 +0100

    Revert "Cleaner workaround"
    This reverts commit ed6833b

--- test/compile-1a.sh
+++ test/compile-1a.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Workaround, see https://github.com/tectonic-typesetting/tectonic/issues/131
-sudo mkdir -p ~/.config/Tectonic/
-echo "[[default_bundles]]" | sudo tee --append ~/.config/Tectonic/config.toml
-sudo echo "url = \"https://tectonic.newton.cx/bundles/tlextras-2018.1r0/bundle.tar\"" | sudo tee --append ~/.config/Tectonic/config.toml
-
 # Copied from 1a-tectonic-docker/.travis.yml without comments
-docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; tectonic biber-mwe.tex; tectonic main.tex"
+# Add explicit bundle link, see https://github.com/tectonic-typesetting/tectonic/issues/131
+docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 --web-bundle \"https://tectonic.newton.cx/bundles/tlextras-2018.1r0/bundle.tar\" biber-mwe.tex; biber biber-mwe; tectonic --web-bundle \"https://tectonic.newton.cx/bundles/tlextras-2018.1r0/bundle.tar\" biber-mwe.tex; tectonic --web-bundle \"https://tectonic.newton.cx/bundles/tlextras-2018.1r0/bundle.tar\" main.tex"
