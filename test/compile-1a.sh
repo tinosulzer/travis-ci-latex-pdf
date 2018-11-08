22b673e62d7d178ba4dc69995e007e8201be6732
Thomas <hollandpirates@gmail.com>
2018-11-08 10:23:51 +0100

    Try building just main

--- test/compile-1a.sh
+++ test/compile-1a.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Copied from 1a-tectonic-docker/.travis.yml without comments
-docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; biber-mwe.tex; main.tex"
+docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic main.tex"
+#docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; biber-mwe.tex; main.tex"
