5ba2076d0428086bf3d10a99031fba4ff13b6f59
Thomas <hollandpirates@gmail.com>
2018-12-08 21:07:04 +0100

    Change directory before sourcing test

--- test/texlive-test.sh
+++ test/texlive-test.sh
@@ -2,6 +2,7 @@
 
 # This file is used for testing this repo, replacing the corresponding .travis.yml which cannot be included.
 
+cd ${TRAVIS_BUILD_DIR}/4-texlive/
 source ${TRAVIS_BUILD_DIR}/4-texlive/texlive/texlive_install.sh
 
 cd ${TRAVIS_BUILD_DIR}/src/
