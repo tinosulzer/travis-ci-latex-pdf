1bd261fa1e4172956ec42bc2a8d8a0c0206c795c
Thomas <hollandpirates@gmail.com>
2019-01-26 11:16:43 +0100

    Re-enable other tests

--- test/texlive-test.sh
+++ test/texlive-test.sh
@@ -8,7 +8,6 @@ cd ${TRAVIS_BUILD_DIR}/4-texlive/
 source ${TRAVIS_BUILD_DIR}/4-texlive/texlive/texlive_install.sh
 
 cd ${TRAVIS_BUILD_DIR}/src/
-#texliveonfly ./main.tex
+texliveonfly ./main.tex
 
-pdflatex --shell-escape ./minted.tex
 texliveonfly --arguments="--shell-escape" ./minted.tex
