d9f94b255129e1bfccbe29f3cdae1881877a7b6c
Thomas <hollandpirates@gmail.com>
2019-01-26 10:40:21 +0100

    Add test for minted package with texlive, see #22

--- test/texlive-test.sh
+++ test/texlive-test.sh
@@ -2,8 +2,12 @@
 
 # This file is used for testing this repo, replacing the corresponding .travis.yml which cannot be included.
 
+sudo apt-get install python-pygments
+
 cd ${TRAVIS_BUILD_DIR}/4-texlive/
 source ${TRAVIS_BUILD_DIR}/4-texlive/texlive/texlive_install.sh
 
 cd ${TRAVIS_BUILD_DIR}/src/
 texliveonfly ./main.tex
+
+texliveonfly --arguments="--shell-escape" ./minted.tex
