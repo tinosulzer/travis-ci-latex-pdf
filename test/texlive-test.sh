659b0d9258516253fdff44dd5c8c3f7ea9869ca2
Thomas <hollandpirates@gmail.com>
2019-01-26 11:05:54 +0100

    Make minted example more minimal

--- test/texlive-test.sh
+++ test/texlive-test.sh
@@ -10,5 +10,5 @@ source ${TRAVIS_BUILD_DIR}/4-texlive/texlive/texlive_install.sh
 cd ${TRAVIS_BUILD_DIR}/src/
 #texliveonfly ./main.tex
 
+pdflatex --shell-escape ./minted.tex
 texliveonfly --arguments="--shell-escape" ./minted.tex
-#pdflatex --shell-escape ./minted.tex
